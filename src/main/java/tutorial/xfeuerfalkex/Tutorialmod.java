8a3e3aeaf9dc5ddcc93684c97d6ac3e3bff05d46
xfeuerfalkex-falke <xfeuerfalkex@gmail.com>
2026-08-07 19:55:53 +0200

    first

---
+++ src/main/java/tutorial/xfeuerfalkex/Tutorialmod.java
@@ -0,0 +1,22 @@
+package tutorial.xfeuerfalkex;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorialmod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
