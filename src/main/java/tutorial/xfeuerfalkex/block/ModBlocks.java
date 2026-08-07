c63ebad8509ae948ad5e218ffc636098522723de
xfeuerfalkex-falke <xfeuerfalkex@gmail.com>
2026-08-08 01:46:43 +0200

    Added Recipes/Smelting
    Loottables done but not working

--- src/main/java/tutorial/xfeuerfalkex/block/ModBlocks.java
+++ src/main/java/tutorial/xfeuerfalkex/block/ModBlocks.java
@@ -3,6 +3,7 @@ package tutorial.xfeuerfalkex.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,6 +11,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import tutorial.xfeuerfalkex.Tutorialmod;
 
 public class ModBlocks {
@@ -20,6 +22,10 @@ public class ModBlocks {
     public static final Block BLUE_FLAMING_BLOCK = registerBlock("blue_flaming_block",
             new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block FIRESTONE_ORE = registerBlock("firestone_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2,5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.STONE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -37,6 +43,10 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.FLAMING_BLOCK);
             entries.add(ModBlocks.BLUE_FLAMING_BLOCK);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(ModBlocks.FIRESTONE_ORE);
         });
     }
 }
