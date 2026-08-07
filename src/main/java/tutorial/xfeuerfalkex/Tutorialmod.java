69c70f8a89932e34b9642e049a4709504cc9f0b0
xfeuerfalkex-falke <xfeuerfalkex@gmail.com>
2026-08-07 20:52:32 +0200

    added 2 items, no attributes yet

--- src/main/java/tutorial/xfeuerfalkex/Tutorialmod.java
+++ src/main/java/tutorial/xfeuerfalkex/Tutorialmod.java
@@ -6,6 +6,7 @@ import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import tutorial.xfeuerfalkex.item.ModItems;
 
 public class Tutorialmod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
@@ -13,10 +14,6 @@ public class Tutorialmod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-	}
-
-	public static Identifier id(String path) {
-		return Identifier.of(MOD_ID, path);
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/tutorial/xfeuerfalkex/item/ModItems.java
@@ -0,0 +1,28 @@
+package tutorial.xfeuerfalkex.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import tutorial.xfeuerfalkex.Tutorialmod;
+
+public class ModItems {
+    public static final Item EPIC_SWORD = registerItem("epic_sword", new Item(new Item.Settings()));
+    public static final Item EPIC_SWORD_FIRE = registerItem("epic_sword_fire", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Tutorialmod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Tutorialmod.LOGGER.info("Registering Mod Items for " + Tutorialmod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(EPIC_SWORD);
+            entries.add(EPIC_SWORD_FIRE);
+        });
+    }
+}
